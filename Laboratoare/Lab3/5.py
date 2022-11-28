908a219b69de3fc4863df1d3b53ca20cf1f21144
Ana Roșu <anarosu766@gmail.com>
2022-11-28 02:10:24 +0200

    added lab3

---
+++ Laboratoare/Lab3/5.py
@@ -0,0 +1,10 @@
+'''Fie un text literar (cu semne de punctuatie, propozitii etc)
+a) sa se afiseze nr cuvv care se incheie cu o vocala
+b) sa se afiseze nr cuv care se incheie cu o vocala din fiecare propozitie
+Ex: s='Ana, ai plecat?...Maria e acasa!'
+a) 5
+b) 2 (pt prima prop)
+   3 (pt a 2 a prop)
+
+Solutie: convertim semnele de punctuatie cu spatii si utilizam split()'''
+s = input('s: ')
